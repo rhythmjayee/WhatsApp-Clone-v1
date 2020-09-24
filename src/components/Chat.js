ea1bd208901cd7ecb3bc6d20c2582b08e9f5dc46
rhythmjayee <jayeerythm8@gmail.com>
2020-09-25 01:46:08 +0530

    chat footer css

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,5 @@
 import {Avatar, IconButton } from '@material-ui/core';
-import { AttachFile,MoreVert,SearchOutlined} from '@material-ui/icons';
+import { AttachFile,MoreVert,SearchOutlined,InsertEmoticon} from '@material-ui/icons';
 import React from 'react';
 import './Chat.css';
 function Chat() {
@@ -34,6 +34,15 @@ function Chat() {
               Message
               <span className="chat__timestamp">{new Date().toUTCString()}</span>
               </p>               
+           </div>
+           <div className="chat__footer">
+            <InsertEmoticon/>
+            <form>
+                <input placeholder="type a mesage" type="text"/>
+                <button type="submit">
+                Send a message
+                </button>
+            </form>
            </div>
         </div>
     )
