2eae295c31217aef27b098b4ce9fe123c70d0a7e
rhythmjayee <jayeerythm8@gmail.com>
2020-09-25 01:31:52 +0530

    chat header & messages css

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,40 @@
+import {Avatar, IconButton } from '@material-ui/core';
+import { AttachFile,MoreVert,SearchOutlined} from '@material-ui/icons';
 import React from 'react';
 import './Chat.css';
 function Chat() {
     return (
         <div className="chat">
-            Chat
+           <div className="chat__header">
+               <Avatar/>
+               <div className="chat__headerInfo">
+                   <h3>Room Name</h3>
+                   <p>Last seen at....</p>
+               </div>
+               <div className="chat__headerRight">
+               <IconButton>
+                  <AttachFile/>
+                </IconButton> 
+                <IconButton>
+                  <SearchOutlined/>
+                </IconButton> 
+                <IconButton>
+                  <MoreVert/>
+                </IconButton>  
+               </div>
+           </div>
+           <div className="chat__body">
+              <p className="chat__message">
+              <span className="chat__name">Rhythm</span>
+              Message
+              <span className="chat__timestamp">{new Date().toUTCString()}</span>
+              </p> 
+              <p className="chat__message chat__reciever">
+              <span className="chat__name">Rhythm</span>
+              Message
+              <span className="chat__timestamp">{new Date().toUTCString()}</span>
+              </p>               
+           </div>
         </div>
     )
 }
