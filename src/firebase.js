4d1c438043e192a9043d4a54b592e9d92399fecd
rhythmjayee <jayeerythm8@gmail.com>
2020-09-24 23:22:25 +0530

    inital setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBV9SBCIi1kRVshZswNgfskVonkdL_mjiU",
+    authDomain: "whatsapp-clone-676f7.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-676f7.firebaseio.com",
+    projectId: "whatsapp-clone-676f7",
+    storageBucket: "whatsapp-clone-676f7.appspot.com",
+    messagingSenderId: "598996848690",
+    appId: "1:598996848690:web:05c25e938709e4b0052c2b"
+  };
